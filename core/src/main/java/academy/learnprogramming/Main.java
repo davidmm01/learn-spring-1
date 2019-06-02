3ba6a6013c27e1374973c12cc3ac2446e47e9fee
David Michieli <d.m.michieli@gmail.com>
2019-06-02 14:20:01 +1000

    s5-23-project-setup

---
+++ core/src/main/java/academy/learnprogramming/Main.java
@@ -0,0 +1,13 @@
+package academy.learnprogramming;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+    // type 'psvm' then tab for auto generation of public static void main
+    public static void main(String[] args) {
+        log.info("Guess The Number Game");
+    }
+}
