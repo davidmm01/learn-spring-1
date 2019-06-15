67e52f4c9cdd6a58468955a5eae517635eb923e9
David Michieli <d.m.michieli@gmail.com>
2019-06-15 20:28:35 +1000

    s5-35 implement remaining message generator logic

--- core/src/main/java/academy/learnprogramming/MessageGeneratorImpl.java
+++ core/src/main/java/academy/learnprogramming/MessageGeneratorImpl.java
@@ -27,11 +27,30 @@ public class MessageGeneratorImpl implements MessageGenerator {
     // public methods
     @Override
     public String getMainMessage() {
-        return "called getMainMessage";
+        return "Number is between " + game.getSmallest() + " and " + game.getBiggest() + ".  Can you guess it?";
     }
 
     @Override
     public String getResultMessage() {
-        return "called getResultMessage";
+        if (game.isGameWon()){
+            return "You guess it! The number was " + game.getNumber();
+        }
+        else if (game.isGameLost()){
+            return "You lost.  The number was " + game.getNumber();
+        }
+        else if (!game.isValidNumberRange()) {
+            return "Invalid number range!";
+        }
+        else if (game.getRemainingGuesses() == guessCount){
+            return "What is your first guess?";
+        }
+        else {
+            String direction = "Lower";
+            if (game.getGuess() < game.getNumber()){
+                direction = "Higher";
+            }
+            return direction + "! You have " + game.getRemainingGuesses() + " guesses left...";
+        }
+
     }
 }
